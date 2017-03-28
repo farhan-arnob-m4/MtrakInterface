96150d35932bc8ed2d3eff7b7ec00d56a1856c72
farhan-arnob-m4 <farhan.arnob@metafour.com>
2017-03-28 13:30:31 +0600

    change FontFace

--- app/src/main/java/com/example/metafour/mtrakinterface/Controller/SiteArrayAdapter.java
+++ app/src/main/java/com/example/metafour/mtrakinterface/Controller/SiteArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.example.metafour.mtrakinterface.Controller;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,9 +30,17 @@ public class SiteArrayAdapter extends ArrayAdapter<SiteDetails> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_layout_for_site_code, parent, false);
         }
+
+
         // Lookup view for data population
         TextView siteName = (TextView) convertView.findViewById(R.id.site_name);
         TextView siteCode = (TextView) convertView.findViewById(R.id.site_code);
+
+        //changing typeface
+        Typeface type = Typeface.createFromAsset(getContext().getAssets(),
+                "fonts/DejaVuSansCondensed-Bold.ttf");
+        siteCode.setTypeface(type);
+        siteName.setTypeface(type);
         // Populate the data into the template view using the data object
         siteName.setText(singleSiteDetails.getSiteName());
         siteCode.setText(singleSiteDetails.getSiteCode());
